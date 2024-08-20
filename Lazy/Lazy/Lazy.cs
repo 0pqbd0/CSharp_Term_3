0bfe193889005e7f7194e750eca0a858b94bf3a9
Artem Mikulskii <st107472@student.spbu.ru>
2024-08-20 23:45:36 +0300

    Task Lazy

---
+++ Lazy/Lazy/Lazy.cs
@@ -0,0 +1,44 @@
+﻿using ILazy;
+namespace LazyImplementation;
+
+public class Lazy<T> : ILazy<T>
+    where T : class, new()
+{
+    private Exception? supplierException;
+    private Func<T>? supplier;
+    private T? value;
+
+    public Lazy(Func<T> supplier)
+    {
+        value = null;
+        this.supplier = supplier;
+    }
+
+    public T Get()
+    {
+        if (supplierException != null)
+        {
+            throw supplierException;
+        }
+        if (supplier == null)
+        {
+            throw new ArgumentNullException("Supplier cannot be null");
+        }
+
+        if (value == null)
+        {
+            try
+            {
+                value = supplier();
+            }
+            catch (Exception ex)
+            {
+                supplierException = ex;
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        return value;
+    }
+
+}
